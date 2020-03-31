ed892b3e5035285ac41ff259b4a9fae0eb264bfc
Krenar Shabani <krenarshabani@gmail.com>
2020-03-31 17:21:28 -0400

    Updates

---
+++ precache-manifest.21af5a9966c7cb20c9b3773a1c5792d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92301cdbb77e25acd7599c4fcec7086",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e0e5eb4b79520f68482f",
+    "url": "/react-gh-pages/static/css/main.6375f091.chunk.css"
+  },
+  {
+    "revision": "1946ef560221cdf1314f",
+    "url": "/react-gh-pages/static/js/2.4059ae0f.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "/react-gh-pages/static/js/2.4059ae0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0e5eb4b79520f68482f",
+    "url": "/react-gh-pages/static/js/main.de0a138e.chunk.js"
+  },
+  {
+    "revision": "be99e9381242de27185d",
+    "url": "/react-gh-pages/static/js/runtime-main.8933936b.js"
+  }
+]);
